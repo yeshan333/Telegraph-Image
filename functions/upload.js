8e92faefe35bebf8ec7cdec043c31b85ddc9138b
yeshan <yeshan.ye@alibaba-inc.com>
2024-03-15 14:26:03 +0800

    fix server error

--- functions/upload.js
+++ functions/upload.js
@@ -1,20 +1,18 @@
 export async function onRequestPost(context) {  // Contents of context object
     const {
         request, // same as existing Worker API
-    env, // same as existing Worker API
-    params, // if filename includes [id] or [[path]]
-     waitUntil, // same as ctx.waitUntil in existing Worker API
-     next, // used for middleware or to fetch assets
-     data, // arbitrary space for passing data between middlewares
-     } = context;
-     context.request
-     const url = new URL(request.url);
-     const response = fetch('https://telegra.ph/' + url.pathname + url.search, {
-         method: request.method,
-         headers: request.headers,
-         body: request.body,
-     });
-    response.headers.set('Access-Control-Allow-Origin', '*');
-    response.headers.set('Access-Control-Max-Age', '86400');
+        env, // same as existing Worker API
+        params, // if filename includes [id] or [[path]]
+        waitUntil, // same as ctx.waitUntil in existing Worker API
+        next, // used for middleware or to fetch assets
+        data, // arbitrary space for passing data between middlewares
+    } = context;
+    context.request
+    const url = new URL(request.url);
+    const response = fetch('https://telegra.ph/' + url.pathname + url.search, {
+        method: request.method,
+        headers: request.headers,
+        body: request.body,
+    });
     return response;
-  }
+}
